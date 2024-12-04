abc0eb6d3b25b4ec96a9a984aab2dd778afdf147
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2024-12-04 09:04:51 +0100

    minor

---
+++ grad_desc_inner.py
@@ -0,0 +1,47 @@
+import numpy as np
+from scipy.ndimage import gaussian_filter1d
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+
+def G(x, y, w):
+    x_1d = np.sort(x.dot(w))
+    y_1d = np.sort(y.dot(w))
+    return np.sum((x_1d - y_1d) ** 2)
+
+def G_eps(x, y, w, epsilon, n_samples):
+    return np.mean([G(x, y, w + epsilon * z) for z in np.random.randn(n_samples)])
+
+def grad_G_eps_Stein(x, y, w, epsilon, n_samples):
+    G0 = G(x, y, w)
+    return np.mean([(G(x, y, w + epsilon * z) - G0) * z / epsilon for z in np.random.randn(n_samples)])
+
+
+n = 30
+n_iter_grad = 30
+learning_rate = 1e-2
+epsilon = .1
+d = 20
+
+np.random.seed(0)
+
+x = np.random.randn(n, d)
+y = np.random.randn(n, d)
+
+w0 = np.random.randn(d, )
+w0 /= np.linalg.norm(w0)
+
+plt.figure()
+for n_samples in [10, 100, 1000]:
+    w = [w0]
+    G_vals = []
+    for _ in range(n_iter_grad):
+        G_vals.append(G(x, y, w[-1]))
+        g = grad_G_eps_Stein(x, y, w[-1], epsilon, n_samples)
+        w.append(w[-1] - learning_rate * g)
+    plt.plot(G_vals, label=f"n_samples={n_samples}")
+plt.legend()
+plt.title(f"Descente de gradient en dimension {d}")
+plt.xlabel("Iterations")
+plt.ylim([0, 10])
+plt.tight_layout()
+plt.show()
